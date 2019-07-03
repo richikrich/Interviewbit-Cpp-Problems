a29b9160617ec0568ef0c651914a2ae07821038a
Richik Raha <49517778+richikrich@users.noreply.github.com>
2019-07-03 22:43:20 +0530

    MAXSPROD - Method 1
    Seems to work fine, but has O(n*n) time complexity, so I'm pretty sure there's a better solution.

---
+++ MAXSPROD - Method 1.cpp
@@ -0,0 +1,56 @@
+#include<iostream>
+#include<vector>
+#include<cmath>
+#include<bits/stdc++.h> 
+
+using namespace std;
+
+int maxsProd(vector<int> &A)
+{
+	int ret=INT_MIN;
+	for(int i=0; i<A.size(); i++)
+	{
+		int a=0,b=0;
+		for(int j=i-1;j>-1;j--)
+		{
+			if(A[i]<A[j])
+			{
+				a=j+1;
+				break;
+			}
+		}
+		for(int j=i+1; j<A.size(); j++)
+		{
+			if(A[j]>A[i])
+			{
+				b=j+1;
+				break;
+			}
+		}
+		if((a*b)>ret) ret=a*b;
+	}
+	return ret;
+}
+
+
+int main()
+{
+	cout<<"Enter the number of inputs in your vector: \n";
+	int n; cin>>n; int temp;
+	vector<int> vect;
+	cout<<"\nEnter: ";
+	for(int i=0; i<n; i++)
+	{
+		cin>>temp;
+		vect.push_back(temp);
+	}
+	cout<<"The vector you entered is: \n";
+	for(int i=0; i<n; i++)
+	{
+		cout<<vect[i]<<" ";
+	}
+	cout<<endl;
+	int result=maxsProd(vect);
+	cout<<"Result: "<<result<<endl;
+	return 0;
+}
